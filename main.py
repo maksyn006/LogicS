7b4f38535523f57f2d5ccabfabb7709511169981
maksyn006 <chudnivakk@gmail.com>
2022-02-07 20:25:54 +0200

    aded my.kv

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.label import Label
-
+from kivy.uix.floatlayout import FloatLayout
+class Container(FloatLayout):
+    pass
 class MyApp(App):
+    def __init__(self):
+        super().__init__()
     def build(self):
-        return Label(text='Hi')
-
+        return Container()
 if __name__ == '__main__':
     MyApp().run()
